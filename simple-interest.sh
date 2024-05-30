292ffd7084d3fd69d43915020588a1a89eab74e0
Khalel Mohamed <75634608+khalelmoh@users.noreply.github.com>
2024-05-30 00:47:20 -0700

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -8,3 +8,24 @@
 8
 9
 10
+
+#!/bin/bash
+* This script calculates simple interest given principal,
+# annual rate of interest and time period in years.
+
+# Author: ‹khalelmoh>
+# Input:
+# p, principal amount
+# t, time period in years
+#r, annual rate of interest
+# Output:
+# simple interest - ptr
+echo "Enter the principal:"
+read p
+echo "Enter rate of interest per year:"
+read r
+echo "Enter time period in years:"
+read t
+s= expr $p 1* $t 1* $r/ 100
+echo "The simple interest is: "
+echo $s
